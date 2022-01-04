59306ed9a707b0ee50498d5ec2a56817c4a0c958
ooooo5h <ooooo5h91@gmail.com>
2022-01-04 10:14:40 +0900

    finish() : 현재 화면 종료(이전화면으로 돌아가기)

--- app/src/main/java/com/neppplus/intent_20220104/OtherActivity.kt
+++ app/src/main/java/com/neppplus/intent_20220104/OtherActivity.kt
@@ -1,11 +1,23 @@
 package com.neppplus.intent_20220104
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        btnReturnToMain.setOnClickListener {
+
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+//            이전 화면으로 복귀는 "현재 화면을 종료"하겠다는 뜻
+            finish()
+
+        }
     }
 }
