9c9d127ca0d7e04e4fd3919e4863c251e7c3a012
ooooo5h <ooooo5h91@gmail.com>
2022-01-04 09:57:52 +0900

    버튼에 이벤트 추가   - 주석만 작성

--- app/src/main/java/com/neppplus/intent_20220104/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20220104/MainActivity.kt
@@ -2,10 +2,17 @@ package com.neppplus.intent_20220104
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnMoveToOther.setOnClickListener {
+
+//            Intent를 이용해서 다른 화면으로 이동
+
+        }
     }
 }
