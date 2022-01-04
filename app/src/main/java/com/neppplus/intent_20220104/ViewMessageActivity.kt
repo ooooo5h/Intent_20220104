40a1daacd052ce4d123c6d672641d77641275dd6
ooooo5h <ooooo5h91@gmail.com>
2022-01-04 10:39:48 +0900

    첨부한 데이터를 받아서 활용   - val mesage = intent.get자료형Extra("이름표")   - txtMessage.text = mesage

--- app/src/main/java/com/neppplus/intent_20220104/ViewMessageActivity.kt
+++ app/src/main/java/com/neppplus/intent_20220104/ViewMessageActivity.kt
@@ -2,12 +2,17 @@ package com.neppplus.intent_20220104
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message.*
 
 class ViewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_message)
 
+//        이전 화면에서 보내준 문구를 찾기
+        val message = intent.getStringExtra("message")
+
+        txtMessage.text = message
 
     }
 }
